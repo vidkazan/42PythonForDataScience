1bc0bb72a5a9d748e993585a486cde56618f4267
Dmitrii Grigorev <vid.kazan@gmail.com>
2024-11-29 15:02:48 +0100

    0/ex00

---
+++ 0/ex00/Hello.py
@@ -0,0 +1,14 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+ft_list[1] = "World!"
+ft_tuple = ("Hello", "Deutschland!")
+ft_set = {"Hello", "Wolfsburg!"}
+ft_dict["Hello"] = "42Wolfburg!"
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
